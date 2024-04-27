7418747a57ed8497e762a7c734fdcfbe8675d0e3
kiran kale <kirannkale358@gmail.com>
2024-04-27 15:56:21 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+
+
+const Login = () => {
+  return (
+    <>
+    
+    <div>
+<h1>Login</h1>
+
+<div className="">
+<input type="text" placeholder="Username" required />
+</div>
+
+<div>
+  <input type="text" placeholder="Password" required />
+</div>
+
+<div>
+  <label> <input type="checkbox"  />Remember me</label>
+  <a href="#">Forgot password</a>
+</div>
+
+<div>
+  <button type="submit">Login</button>
+
+  <div>
+    <p>Don't have an account? <a href="#">Register</a></p>
+  </div>
+</div>
+    </div>
+    </>
+  )
+}
+
+export default Login
